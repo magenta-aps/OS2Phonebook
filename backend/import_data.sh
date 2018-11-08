182782b3c9a47ce4c31464c1ff2957f8c3499f1b
Ubuntu <carstena@magenta.dk>
2018-11-08 18:32:55 +0000

    Only one import script.

--- backend/import_data.sh
+++ backend/import_data.sh
@@ -6,5 +6,24 @@ MO_URL=http://morademo.atlas.magenta.dk/service
 MO_ORG_ROOT=293089ba-a1d7-4fff-a9d0-79bd8bab4e5b
 
 # Remove old exported files, if any
-rm -rf $VAR_DIR/*
+rm -rf $VAR_DIR/ous/*
+rm -rf $VAR_DIR/employees/*
+
+# Do the deed
 MO_URL=$MO_URL MO_ORG_ROOT=$MO_ORG_ROOT $PYTHON $PROGRAM
+
+# Now transfer to SOLR
+
+SOLR_POST=solr-7.5.0/bin/post
+
+# First, clean index for existing data.
+$SOLR_POST -c departments -d "<delete><query>*:*</query></delete>"
+$SOLR_POST -c employees -d "<delete><query>*:*</query></delete>"
+
+# Reindex
+$SOLR_POST -c departments $VAR_DIR/ous
+$SOLR_POST -c employees $VAR_DIR/employees
+
+# Clean up after yourself.
+rm -rf $VAR_DIR/ous/*
+rm -rf $VAR_DIR/employees/*
