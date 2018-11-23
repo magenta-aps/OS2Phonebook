ad7f81edb279c709cced386f8b4721b99111901c
Carsten Agger <carstena@magenta.dk>
2018-11-23 10:58:41 +0100

    Forgot debug change.

--- backend/clean.sh
+++ backend/clean.sh
@@ -6,4 +6,4 @@ cd $(dirname $0)
 solr-7.5.0/bin/solr stop
 
 # Remove everything that's not source.
-rm -r solr-7.5.0/ var/ import/__pycache__/ venv/ build/ coverage.xml
+rm -rf solr-7.5.0/ var/ import/__pycache__/ venv/ build/ coverage.xml
