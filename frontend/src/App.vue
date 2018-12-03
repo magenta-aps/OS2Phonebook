81fbf96332fba09fd7bda5e8349b716a7c1387aa
Novik <ns@magenta.dk>
2018-12-03 14:06:59 +0100

    Changed styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
     <router-link :to="{ name: 'home'}">
-      <b-navbar class="bg-primary">
-        <b-navbar-brand class="mx-auto"><span class="text-color">{{ $t('phonebook') }}</span></b-navbar-brand>
+      <b-navbar class="bg-primary" type="dark">
+        <b-navbar-brand class="mx-auto">{{ $t('phonebook') }}</b-navbar-brand>
       </b-navbar>
     </router-link>
 
-    <main class="container container-fluid container-top-margin">
+    <main class="container container-fluid mt-4">
       <router-view/>
     </main>
   </div>
@@ -16,13 +16,5 @@
   #app {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-  }
-
-  .container-top-margin {
-    margin-top: 1em;
-  }
-
-  .text-color {
-    color: #ffffff;
   }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store/index'
+import store from './store'
 import i18n from './i18n'
 import BootstrapVue from 'bootstrap-vue'
 import 'vue-awesome/icons'
