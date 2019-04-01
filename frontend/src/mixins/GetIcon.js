9f8412c7a4f02bec34c162520e8329955e08b3e2
Novik <ns@magenta.dk>
2019-04-01 11:21:30 +0200

    Added new icons

--- frontend/src/mixins/GetIcon.js
+++ frontend/src/mixins/GetIcon.js
@@ -7,6 +7,14 @@ export default {
         return 'map-marker-alt'
       } else if (locationType === 'EMAIL') {
         return 'envelope'
+      } else if (locationType === 'WWW') {
+        return 'globe'
+      } else if (locationType === 'EAN') {
+        return 'list-ol'
+      } else if (locationType === 'PNUMBER') {
+        return 'list-ol'
+      } else if (locationType === 'TEXT') {
+        return 'book'
       }
       return null
     }
