6cbeb0f850c96a210787deba96af859ee7e8420a
Ubuntu <carstena@magenta.dk>
2019-06-19 13:06:04 +0000

    Add new EXCLUDE env variable.

--- backend/config.sh
+++ backend/config.sh
@@ -1,7 +1,8 @@
 
-export MO_URL=https://os2mo-dev.ballerup.dk/service
-export MO_ORG_ROOT=71c6bb39-88cb-45cc-8c8b-1608d959bb81
-export API_TOKEN=ba10dd03-c1dc-4871-a3d0-a1f88bd62081
-export IMPORT_LOG_FILE=$(dirname $0)/var/import_log.txt
-export SOLR_POST=/home/ubuntu/os2phonebook/backend/solr-7.7.2/bin/post
+MO_URL=https://morademo.atlas.magenta.dk/service
+MO_ORG_ROOT=71c6bb39-88cb-45cc-8c8b-1608d959bb81
+API_TOKEN=ba10dd03-c1dc-4871-a3d0-a1f88bd62081
+IMPORT_LOG_FILE=$(dirname $0)/var/import_log.txt
+SOLR_POST=/home/ubuntu/os2phonebook/backend/solr-7.7.2/bin/post
+EXCLUDE=9B-Top
 
