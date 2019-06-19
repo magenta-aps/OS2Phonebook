a3fa7c59ea00aa8a8628d0e0fd01f6e74e22c9f4
Ubuntu <carstena@magenta.dk>
2019-06-19 13:10:26 +0000

    Fix some oversights.

--- backend/config.sh
+++ backend/config.sh
@@ -1,8 +1,9 @@
 
-MO_URL=https://morademo.atlas.magenta.dk/service
+MO_URL=http://morademo.atlas.magenta.dk/service
 MO_ORG_ROOT=71c6bb39-88cb-45cc-8c8b-1608d959bb81
 API_TOKEN=ba10dd03-c1dc-4871-a3d0-a1f88bd62081
 IMPORT_LOG_FILE=$(dirname $0)/var/import_log.txt
+SOLR_URL=http://localhost:8983/solr
 SOLR_POST=/home/ubuntu/os2phonebook/backend/solr-7.7.2/bin/post
 EXCLUDE=9B-Top
 
